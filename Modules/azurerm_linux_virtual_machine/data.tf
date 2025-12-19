d337aa01e81afa8101cd395445c251a02adcf271
mohitdhiman80 <mohitdhiman.hcl@gmail.com>
2025-12-19 16:09:53 +0530

    added vm and net interface

---
+++ Modules/azurerm_linux_virtual_machine/data.tf
@@ -0,0 +1,5 @@
+data "azurerm_network_interface" "this" {
+  for_each = var.linux_virtual_machines
+  name                = each.value.network_interface_name
+  resource_group_name = each.value.resource_group_name
+}
---
+++ Modules/azurerm_network_Interface/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_network_interface" "md-net" {
+  for_each            = var.network_interfaces
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  dynamic "ip_configuration" {
+    for_each = each.value.ip_configurations
+    content {
+      name                          = ip_configuration.value.name
+      subnet_id                     = data.azurerm_subnet.md-net[each.key].id
+      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation
+    }
+
+  }
+}
