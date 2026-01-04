6fbe5dc436262d40e2c55fef34af88f066358a8c
mohitdhiman80 <mohitdhiman.hcl@gmail.com>
2026-01-04 15:10:11 +0530

    added linting

--- Environment/dev/main.tf
+++ Environment/dev/main.tf
@@ -3,11 +3,11 @@ module "resource_group" {
   resource_groups = var.resource_groups
 }
 
-module "storage_account" {
-  depends_on = [ module.resource_group ]
-  source                  = "../../Modules/azurerm_storage_account"
-  storage_account = var.storage_account
-  }
+# module "storage_account" {
+#   depends_on = [ module.resource_group ]
+#   source                  = "../../Modules/azurerm_storage_account"
+#   storage_account = var.storage_account
+#   }
 
 module "virtual_network" {
   depends_on = [ module.resource_group ]
@@ -15,30 +15,30 @@ module "virtual_network" {
   virtual_networks = var.virtual_networks
 }
 
-module "network_interface" {
-  depends_on = [ module.virtual_network ]
-  source                = "../../Modules/azurerm_network_interface"
-  network_interfaces    = var.network_interfaces
-}
+# module "network_interface" {
+#   depends_on = [ module.virtual_network ]
+#   source                = "../../Modules/azurerm_network_interface"
+#   network_interfaces    = var.network_interfaces
+# }
 
-module "linux_virtual_machine" {
-  depends_on = [ module.network_interface ]
-  source                = "../../Modules/azurerm_linux_virtual_machine"
-  linux_virtual_machines = var.linux_virtual_machines
+# module "linux_virtual_machine" {
+#   depends_on = [ module.network_interface ]
+#   source                = "../../Modules/azurerm_linux_virtual_machine"
+#   linux_virtual_machines = var.linux_virtual_machines
  
-}
+# }
 
-module "public_ip" {
-  depends_on = [ module.resource_group ]
-  source                = "../../Modules/azurerm_pip"
-  public_ips = var.public_ips
-}
+# module "public_ip" {
+#   depends_on = [ module.resource_group ]
+#   source                = "../../Modules/azurerm_pip"
+#   public_ips = var.public_ips
+# }
 
-module "azurerm_bastion_host" {
-  depends_on = [ module.virtual_network, module.public_ip, module.resource_group ]
-  source                = "../../Modules/azurerm_bastion"
-  bastion_hosts = var.bastion_hosts
-}
+# module "azurerm_bastion_host" {
+#   depends_on = [ module.virtual_network, module.public_ip, module.resource_group ]
+#   source                = "../../Modules/azurerm_bastion"
+#   bastion_hosts = var.bastion_hosts
+# }
 
 module "kubernetes_cluster" {
   depends_on = [ module.resource_group ]
@@ -52,11 +52,11 @@ module "container_registries" {
   container_registries  = var.container_registries
 }
 
-module "key_vault" {
-  depends_on = [ module.resource_group ]
-  source            = "../../Modules/azurerm_keyvault"
-  key_vaults = var.key_vaults 
-}
+# module "key_vault" {
+#   depends_on = [ module.resource_group ]
+#   source            = "../../Modules/azurerm_keyvault"
+#   key_vaults = var.key_vaults 
+# }
 
 module "mssql_server" {
   depends_on = [ module.resource_group ]
